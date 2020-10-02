e988f93970978ef12e05d85d86b7cb1df664b36a
farooq43 <70007522+farooq43@users.noreply.github.com>
2020-10-03 00:09:33 +0500

    Add files via upload

---
+++ mafia-killer-007.py
@@ -0,0 +1,71 @@
+[H[2J[3J
+[1;91m   ______  [1;95m  _____ __     __)[1;92m ______
+ [1;91m (, /    )[1;97m (, /  (, /|  / [1;92m  (, /    )
+ [1;91m   /---(    [1;93m /     / | /  [1;92m    /    /
+[1;91m ) / ____)[1;94m___/__ ) /  |/   [1;92m  _/___ /_
+[1;91m(_/ (   (__[1;96m /   (_/   '   [1;92m (_/___ /                          
+[3;90m
+      FACEBOOK ACCOUNT CLONER BY [3;91mBOTOL BABA[0;37;40m
+[1;94m──────────────────────────────────────────────────
+[1;92m▸ [1;95mAUTHOR     : MEHEDI HASAN ARIYAN
+[1;92m▸ [1;95mFACEBOOK   : FACEBOOK.COM/THEMEHTAN
+[1;92m▸ [1;95mYOUTUBE    : YOUTUBE.COM/MASTERTRICK1
+[1;92m▸ [1;95mGITHUB     : GITHUB.COM/BOTOLMEHEDI
+[1;94m──────────────────────────────────────────────────
+
+[1;94m                     [1;92mTEAM V-VIRUS 
+[1;94m                     [1;92m       
+[1;94m                     [1;92mTEAM V-VIRUS 
+[1;94m                     [1;92m       
+[1;94m                     [1;92mTEAM V-VIRUS 
+[1;94m                     [1;92m       
+[1;94m                     [1;92mTEAM V-VIRUS 
+[1;94m                     [1;92m       
+[1;94m                     [1;92mTEAM V-VIRUS 
+[1;94m                     [1;92m       
+[1;94m                     [1;92mTEAM V-VIRUS 
+[1;94m                     [1;92m       
+[1;94m                     [1;92mTEAM V-VIRUS 
+[1;94m                     [1;92m       
+[1;94m                     [1;92mTEAM V-VIRUS 
+[1;94m                     [1;92m       
+[1;94m                     [1;92mTEAM V-VIRUS 
+[1;94m                     [1;92m       
+[1;94m                     [1;92mTEAM V-VIRUS 
+[1;94m                     [1;92mTEAM V-VIRUS 
+[1;94m                     [1;92m       
+[1;94m                     [1;92mTEAM V-VIRUS 
+[1;94m                     [1;92m       
+[1;94m                     [1;92mTEAM V-VIRUS 
+[1;94m                     [1;92m       
+[1;94m                     [1;92mTEAM V-VIRUS 
+[1;94m                     [1;92m       
+[1;94m                     [1;92mTEAM V-VIRUS 
+[1;94m                     [1;92m       
+[1;94m                     [1;92mTEAM V-VIRUS 
+[1;94m                     [1;92m       
+[1;94m                     [1;92mTEAM V-VIRUS 
+[1;94m                     [1;92m       
+[1;94m                     [1;92mTEAM V-VIRUS 
+[1;94m                     [1;92m       
+[1;94m                     [1;92mTEAM V-VIRUS 
+[1;94m                     [1;92m       
+[1;94m                     [1;92mTEAM V-VIRUS
+
+       [4;96mINDIAN MENU[0;37;40m                 [4;96mINTERNATIONAL[0;37;40m
+
+[1;91m[[1;92m1[1;91m]  [1;93mINDIAN 6 DIGIT CLONE     [1;91m[[1;92m5[1;91m]  [1;93mUSA 6 DIGIT CLONE
+[1;91m[[1;92m2[1;91m]  [1;93mINDIAN 7 DIGIT CLONE     [1;91m[[1;92m6[1;91m]  [1;93mITALY 6 DIGIT CLONE
+[1;91m[[1;92m3[1;91m]  [1;93mFACEBOOK AUTO REPORT     [1;91m[[1;92m7[1;91m]  [1;93mSPAIN 6 DIGIT CLONE
+[1;91m[[1;92m4[1;91m]  [1;93mFRIEND LIST ID HACK      [1;91m[[1;92m8[1;91m]  [1;93mPOLAND 6 DIGIT CLONE
+[1;91m[[1;92m9[1;91m]  [1;93mTARGET ID HACK {BRUTE} 
+
+     [4;96mBANGLADESHI MENU[0;37;40m                [4;96mMORE TOOLS[0;37;40m
+
+[1;91m[[1;92m11[1;91m]  [1;90m6 DIGIT CRACKER         [1;91m[[1;92m15[1;91m]  [1;90mEMAIL CRACKER
+[1;91m[[1;92m12[1;91m]  [1;90m7 DIGIT CRACKER         [1;91m[[1;92m16[1;91m]  [1;90mSMS CALL BOMBER
+[1;91m[[1;92m13[1;91m]  [1;90m8 DIGIT CRACKER         [1;91m[[1;92m17[1;91m]  [1;90mWEBSITE DEFACEMENT
+[1;91m[[1;92m14[1;91m]  [1;90m11 DIGIT CRACKER        [1;91m[[1;92m18[1;91m]  [1;90mDEATH MAIL GRABBER
+
+
+    [1;91m[[1;93m20[1;91m] [1;92mUPDATE [1;91m[[1;93m21[1;91m] [1;92mFOLLOW [1;91m[[1;93m22[1;91m] [1;92mCONTACT [1;91m[[1;93m00[1;91m] [1;92mEXIT
